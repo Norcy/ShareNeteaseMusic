3afd12d7abbd8c8de9a838e09a6fff7ca521370e
Norcy <chenyg32@gmail.com>
2018-10-29 16:29:19 +0800

    Add Node Js

--- SyncMusic.php
+++ SyncMusic.php
@@ -5,9 +5,14 @@
 	syncMusic();
 	function syncMusic()
 	{
-		$cmd = "python3 ./ShareNetEaseMusic.py";
+		$cmd0 = "killall node";
+		//var_dump(shell_exec($cmd0));
+		$cmd1 = "node ../NeteaseCloudMusicApi/app.js";
+		var_dump(shell_exec($cmd1));
+		$cmd2 = "python3 /var/www/html/ShareNeteaseMusic/ShareNetEaseMusic.py";
 		//$cmd = "python3 /var/www/html/Norcy.github.io/isee.py ".$objectName." ".$type." ".$year." ".$mouth." ".$date." 2>&1";
-		var_dump(shell_exec($cmd));
+		var_dump(shell_exec($cmd2));
+		//var_dump(shell_exec($cmd0));
 	    	//shell_exec($cmd);
 	}
 ?>
